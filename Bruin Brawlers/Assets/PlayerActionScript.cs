e98d542ddcd2afd4fcaf01272df85233671cb2a4
patrickparseg <patrickparseg23@gmail.com>
2024-03-14 13:00:48 -0700

    Added 10 second supermove timer. Users now cannot call the supermove while the supermove is activated.

--- Bruin Brawlers/Assets/PlayerActionScript.cs
+++ Bruin Brawlers/Assets/PlayerActionScript.cs
@@ -19,6 +19,10 @@ public class PlayerActionScript : MonoBehaviour
 
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    private Vector3 originalScale;
+    private Color originalColor;
+    private bool activeSM = false;
+    private bool cooldownSM = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +31,14 @@ public class PlayerActionScript : MonoBehaviour
         lastSwitch = 0;
         lastMove = "";
 
-        actions.Add("bombastic", superMove);
+        actions.Add("bombastic", () => superMove());
 
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
         keywordRecognizer.Start();
+
+        originalScale = myRigidBody.transform.localScale;
+        originalColor = GetComponent<SpriteRenderer>().color;
     }
     private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
     {
@@ -88,6 +95,11 @@ public class PlayerActionScript : MonoBehaviour
         {
             Debug.Log("BLOCK!");
         }
+        if (activeSM)
+        {
+            StartCoroutine(SuperMoveCoroutine());       //if keyword is recognized start coroutine and set active to false
+            activeSM = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -122,7 +134,23 @@ public class PlayerActionScript : MonoBehaviour
 
     private void superMove()
     {
+        if (!cooldownSM)
+        {
+            activeSM = true;
+            cooldownSM = true;
+        }
+    }
+
+    private IEnumerator SuperMoveCoroutine()
+    {
         myRigidBody.transform.localScale += new Vector3(1f, 1f, 1f);
         GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 1);
+
+        yield return new WaitForSeconds(10f);   //wait for 10 seconds
+
+        myRigidBody.transform.localScale = originalScale;
+        GetComponent<SpriteRenderer>().color = originalColor;
+
+        cooldownSM = false;
     }
 }
