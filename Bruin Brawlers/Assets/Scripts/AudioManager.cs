371591f80b3f3ae8ff7793df96e195aaaee11783
Raiun <ryan.man821@gmail.com>
2024-04-18 14:26:14 -0700

    added background music

---
+++ Bruin Brawlers/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip hitEffect;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void playHitSound(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
