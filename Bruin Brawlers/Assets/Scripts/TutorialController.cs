a6d13a1ce6b92f06b5d52432e2f83641b270dcf0
patrickparseg <patrickparseg23@gmail.com>
2024-05-30 20:19:25 -0700

    added images to tutorial script

--- Bruin Brawlers/Assets/Scripts/TutorialController.cs
+++ Bruin Brawlers/Assets/Scripts/TutorialController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
 public class TutorialController : MonoBehaviour
@@ -14,6 +15,10 @@ public class TutorialController : MonoBehaviour
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
     private string[] instructions = new string[] { "Move Forward!", "Move Backward!", "Throw a Punch!", "Block!", "Kick!", "Throw Your Strongest Punch!"};
+
+    public Image PunchImg;
+    public Image BlockImg;
+    public Image KickImg;
 
     private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
     {
@@ -27,8 +32,33 @@ public class TutorialController : MonoBehaviour
         if (tutIndex < instructions.Length - 1)
         {
             tutInstruction.text = instructions[tutIndex];
+            UpdateImage();
         }
         Debug.Log(instructions.Length);
+    }
+
+    private void UpdateImage()
+    {
+        PunchImg.gameObject.SetActive(false);
+        BlockImg.gameObject.SetActive(false);
+        KickImg.gameObject.SetActive(false);
+
+
+        if (tutIndex >= 0 && tutIndex < instructions.Length)
+        {
+            switch (instructions[tutIndex])
+            {
+                case "Throw a Punch!":
+                    PunchImg.gameObject.SetActive(true);
+                    break;
+                case "Block!":
+                    BlockImg.gameObject.SetActive(true);
+                    break;
+                case "Kick!":
+                    KickImg.gameObject.SetActive(true);
+                    break;
+            }
+        }
     }
 
     // Start is called before the first frame update
@@ -41,6 +71,10 @@ public class TutorialController : MonoBehaviour
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
         keywordRecognizer.Start();
+
+        PunchImg.gameObject.SetActive(false);
+        BlockImg.gameObject.SetActive(false);
+        KickImg.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
